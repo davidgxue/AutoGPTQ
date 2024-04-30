004a1410dce0c0a87a3472df2352ba2970d1990d
David Xue <xuegdxw@gmail.com>
2024-04-30 13:10:03 -0400

    enable logging propagation

--- auto_gptq/modeling/_base.py
+++ auto_gptq/modeling/_base.py
@@ -71,7 +71,7 @@ logger = logging.getLogger(__name__)
 handler = logging.StreamHandler()
 formatter = logging.Formatter("%(levelname)s - %(message)s")
 handler.setFormatter(formatter)
-logger.propagate = False
+logger.propagate = True
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
